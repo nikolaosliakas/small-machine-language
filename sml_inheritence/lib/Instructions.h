59b315887bb75b9773ac8403f742de53019f582a
nikos_liakas <nikosliakas1010@gmail.com>
2026-06-11 10:30:26 +0100

    Added basic instructions to Instructions - still need to include no operation label jump instruction, Invoking an d inequalities

--- sml_inheritence/lib/Instructions.h
+++ sml_inheritence/lib/Instructions.h
@@ -2,7 +2,7 @@
 #define SMALL_MACHINE_LANGUAGE_INSTRUCTIONS_H
 #pragma once
 #include <string>
-#include <utility>
+
 
 // Forward declaration — enough for the signature inside the struct
 class Interpreter;
@@ -18,17 +18,110 @@ public:
     std::string& operand() { return operand_; }
 };
 
-struct PushInstr : public Instruction {
-    using Instruction::Instruction;
+// Operation on operand stack
+struct PushInstr :  Instruction {
+    explicit PushInstr(const std::string& v) : Instruction(v){}
     std::string name() const override { return "push"; }
-    int execute(Interpreter& vm, int pc) override; // Body deferred
+    int execute(Interpreter& vm, int pc) override {
+        vm.push(std::stoi(operand_));
+        return pc + 1;
+    }
+};
+struct PopInstr :  Instruction {
+    // no need for constructor, default constructor used
+    std::string name() const override { return "pop"; }
+    int execute(Interpreter& vm, int pc) override {
+        vm.pop();
+        return pc + 1;
+    }
+};
+// Operations on loading and retrieving variables from symb table
+struct LoadInstr : Instruction{
+    explicit LoadInstr(const std::string& v) : Instruction(v){}
+    std::string name() const override { return "load"; }
+    int execute(Interpreter& vm, int pc) override {
+        // loads the symboltable stored value and pushes that int onto the operand stack
+        vm.push(vm.loadVar(operand_));
+        return pc + 1;
+    }
+};
+
+struct StoreInstr : Instruction{
+    explicit StoreInstr(const std::string& v) : Instruction(v){}
+    std::string name() const override { return "store"; }
+    int execute(Interpreter& vm, int pc) override {
+        // take top from the operand stack and store in the variable arg passed to operand_
+        vm.store(operand_, vm.pop());
+        return pc + 1;
+    }
+};
+
+// Arithmatic Operations
+struct AddInstr: Instruction{
+    std::string name() const override {return "add";}
+    int execute(Interpreter& vm, int pc) override{
+        int b = vm.pop();
+        int a = vm.pop();
+        vm.push(a + b);
+        return pc + 1;
+    }
+};
+struct SubInstr: Instruction{
+    std::string name() const override {return "sub";}
+    int execute(Interpreter& vm, int pc) override{
+        int b = vm.pop();
+        int a = vm.pop();
+        vm.push(a - b);
+        return pc + 1;
+    }
+};
+struct MulInstr: Instruction{
+    std::string name() const override {return "mul";}
+    int execute(Interpreter& vm, int pc) override{
+        int b = vm.pop();
+        int a = vm.pop();
+        vm.push(a * b);
+        return pc + 1;
+    }
+};
+
+struct DivInstr: Instruction{
+    std::string name() const override {return "div";}
+    int execute(Interpreter& vm, int pc) override{
+        int b = vm.pop();
+        int a = vm.pop();
+        if (b == 0) throw std::runtime_error("Division by zero!");
+        vm.push(a / b);
+        return pc + 1;
+    }
+};
+
+struct RetInstr: Instruction{
+    // This is where nesting occurs - the program either transfers to another Frame if ther eis one on the stack
+    //  or it is finished
+    std::string name() const override {return "return";}
+    int execute(Interpreter& vm, int pc) override{
+        bool hasVal = !vm.topFrame().stackEmpty();
+        int retVal = hasVal ? vm.pop() : 0; // either return a value or 0 - expand later to Some/None handling?
+        int nextPC = vm.leave(); // handy leaving the frame calls the return address to begin the program again after the return
+
+        // test if exiting programming
+        if (!vm.hasFrame()){
+            // @main - done
+            vm.setRunning(false);
+            return nextPC;
+        }
+        if (hasVal) vm.push(retVal);
+        return nextPC;
+    }
 };
 
 
-// ---- Define the body down here, inline ----
-inline int PushInstr::execute(Interpreter& vm, int pc) {
-    vm.push(std::stoi(operand_));
-    return pc + 1;
-}
+
+
+
+
+
+
 
 #endif //SMALL_MACHINE_LANGUAGE_INSTRUCTIONS_H
